6b1b6d829e072c3922f72d9c1811f09834542bdf
trungduonb5 <55185282+ShShee@users.noreply.github.com>
2022-04-18 21:33:05 +0700

    [DEV} - Group words

--- test.py
+++ test.py
@@ -9,14 +9,32 @@ from generate_data import data_type
 #     print(reduce_word(pos_tag(handle_in_query(item))), end="\n\n")
 
 query = reduce_word(pos_tag(handle_in_query(query_data[0])))
+skip = 0
+words = []
+list_query = []
 for item in query:
     list_same_names = []
+    if skip == 0:
+        words = [item[0]]
+    else:
+        words.append(item[0])
+        skip = skip - 1
+
     for type in data_type:
-        for name in type.getSameNames(item[0]):
+        for name in type.getSameNames(' '.join(words)):
             if(name):
                 list_same_names.append(name)
-    print(item[0], "------", list_same_names)
 
+    count_inclued = len(list_same_names)
+    if(count_inclued == 0):
+        for word in words:
+            list_query.append(word)
+    elif (count_inclued == 1 and ' '.join(words) == list_same_names[0]):
+        list_query.append(' '.join(words))
+    else:
+        skip = skip+1
+
+print(list_query)
 # list_same_names = []
 # for type in data_type:
 #     for name in type.getSameNames(query[-1][0]):
