62ab9efa7552a05f89a4db21099d82b820c0b4c1
trungduonb5 <55185282+ShShee@users.noreply.github.com>
2022-04-28 17:09:53 +0700

    [DEV] - Add some data

--- test.py
+++ test.py
@@ -6,6 +6,3 @@ from query import query_data
 from generate_data import data_type
 
 reduce_words(query_data)
-# for index in range(0, len(list_query)-1):
-#     print(list_query[index][0], "-", list_query[index+1][0],
-#           '- Connector:', define_connection(list_query[index], list_query[index+1]))
